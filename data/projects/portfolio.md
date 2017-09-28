83401789e9c067211391967e0638aae0944839cd
kanishkarj <kanishkarj@hotmail.com>
2017-09-29 02:29:43 +0530

    added project

---
+++ data/projects/portfolio.md
@@ -0,0 +1,38 @@
+## About this project
+
+------
+
+An portfolio built by me for me. The gulp js config and file structure was forked from another opensource project. Vue js has been used for DOM manipulation and project scalablity. Gulp js has been used to automate tasks and to enhance workflow. This is an Opensource project, hence anyone else can use the template.
+
+The UI/UX was conceived with a **"mobile first"** approach. So the website can be viewed on any screen size with ease.
+
+## Technical Sheet
+
+Code technologies I got involved with while working on this project.
+
+------
+
+- UX Design
+
+- UX Architecture
+
+- UI Animations
+
+- HTML5
+
+- CSS3 – preprocessed with SASS
+
+- Vue Js
+
+- Responsive Web Design
+
+- Gulp JS
+
+  ​
+
+## Resources
+
+------
+
+- The project is online at <https://kanishkarj.github.io>
+- The Code is available at <https://github.com/kanishkarj/kanishkarj.github.io>
--- js/index.js
+++ js/index.js
@@ -10,7 +10,34 @@ var projectList = [
 
       {
         title: 'Responsive Web Design',
+        link: '#'
+      },
+      {
+        title: 'Vue JS',
         link: 'https://vuejs.org'
+      },
+      {
+        title: 'Sass',
+        link: 'http://sass-lang.com/'
+      },
+      {
+        title: 'Gulp Js',
+        link: 'https://gulpjs.com/'
+      },
+    ],
+
+  },
+  {
+    img: 'img/projects/portfolio(bg).png',
+    imgalt: 'img',
+    title: 'kanishkarj.github.io',
+    modalhref: '#projectModal2',
+    modalhreftitle: 'projectModal2',
+    desc: '<h2 id="aboutthisproject">About this project</h2><hr /><p>An portfolio built by me for me. The gulp js config and file structure was forked from another opensource project. Vue js has been used for DOM manipulation and project scalablity. Gulp js has been used to automate tasks and to enhance workflow. This is an Opensource project, hence anyone else can use the template.</p><p>The UI/UX was conceived with a <strong>"mobile first"</strong> approach. So the website can be viewed on any screen size with ease.</p><h2 id="technicalsheet">Technical Sheet</h2><p>Code technologies I got involved with while working on this project.</p><hr /><ul><li><p>UX Design</p></li><li><p>UX Architecture</p></li><li><p>UI Animations</p></li><li><p>HTML5</p></li><li><p>CSS3 – preprocessed with SASS</p></li><li><p>Vue Js</p></li><li><p>Responsive Web Design</p></li><li><p>Gulp JS</p><p>​</p></li></ul><h2 id="resources">Resources</h2><hr /><ul><li>The project is online at <a href="https://kanishkarj.github.io">https://kanishkarj.github.io</a></li><li>The Code is available at <a href="https://github.com/kanishkarj/kanishkarj.github.io">https://github.com/kanishkarj/kanishkarj.github.io</a></li></ul>',
+    tech: [
+      {
+        title: 'Responsive Web Design',
+        link: '#'
       },
       {
         title: 'Vue JS',
